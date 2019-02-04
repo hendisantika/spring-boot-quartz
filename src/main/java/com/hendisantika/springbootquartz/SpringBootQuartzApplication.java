950ff20bfa57c61961e06525d6fa7825b92fedb1
hendisantika <hendisantika@yahoo.co.id>
2019-02-04 18:30:09 +0700

    #5 - Still error in Main Class

--- src/main/java/com/hendisantika/springbootquartz/SpringBootQuartzApplication.java
+++ src/main/java/com/hendisantika/springbootquartz/SpringBootQuartzApplication.java
@@ -1,14 +1,33 @@
 package com.hendisantika.springbootquartz;
 
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.TriggerUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringBootQuartzApplication {
+    @Autowired
+    private Scheduler scheduler;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootQuartzApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() throws SchedulerException {
+        JobDetail jobDetail = QuartzJob.builder()
+                .name("An example Job")
+                .jobClass(ExampleJob.class)
+                .putJobData("url", "http://foo.bar")
+                .build();
+
+        scheduler.scheduleJob(jobDetail, TriggerUtils.createSingleshotTrigger("exampleTrigger", 100L));
+    }
 }
 
