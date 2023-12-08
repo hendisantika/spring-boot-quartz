46ccebb2c15689e689ce97ca03160680a3d27cf2
hendisantika <hendisantika@yahoo.co.id>
2023-12-08 09:59:33 +0700

    #122 - Add schedulerFactoryBean in SchedulerConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootquartz/config/SchedulerConfig.java
+++ src/main/java/com/hendisantika/springbootquartz/config/SchedulerConfig.java
@@ -3,9 +3,12 @@ package com.hendisantika.springbootquartz.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +29,20 @@ public class SchedulerConfig {
     private final ApplicationContext applicationContext;
 
     private final QuartzProperties quartzProperties;
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+        SchedulerJobFactory jobFactory = new SchedulerJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+
+        Properties properties = new Properties();
+        properties.putAll(quartzProperties.getProperties());
+
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setOverwriteExistingJobs(true);
+        factory.setDataSource(dataSource);
+        factory.setQuartzProperties(properties);
+        factory.setJobFactory(jobFactory);
+        return factory;
+    }
 }
