1576c935752001116773bc53970f32c9c02b574c
hendisantika <hendisantika@yahoo.co.id>
2023-12-08 09:57:07 +0700

    #118 - Add createJob in JobScheduleCreator 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootquartz/component/JobScheduleCreator.java
+++ src/main/java/com/hendisantika/springbootquartz/component/JobScheduleCreator.java
@@ -1,6 +1,11 @@
 package com.hendisantika.springbootquartz.component;
 
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,4 +21,20 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class JobScheduleCreator {
+    public JobDetail createJob(Class<? extends QuartzJobBean> jobClass, boolean isDurable,
+                               ApplicationContext context, String jobName, String jobGroup) {
+        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
+        factoryBean.setJobClass(jobClass);
+        factoryBean.setDurability(isDurable);
+        factoryBean.setApplicationContext(context);
+        factoryBean.setName(jobName);
+        factoryBean.setGroup(jobGroup);
+
+        // set job data map
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put(jobName + jobGroup, jobClass.getName());
+        factoryBean.setJobDataMap(jobDataMap);
+        factoryBean.afterPropertiesSet();
+        return factoryBean.getObject();
+    }
 }
