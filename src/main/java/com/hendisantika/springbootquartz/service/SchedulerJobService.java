8d4b7437b0ec7a00f082326a96195c559827855f
hendisantika <hendisantika@yahoo.co.id>
2023-12-08 10:05:10 +0700

    #131 - Add SchedulerJobService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springbootquartz/service/SchedulerJobService.java
@@ -0,0 +1,38 @@
+package com.hendisantika.springbootquartz.service;
+
+import com.hendisantika.springbootquartz.component.JobScheduleCreator;
+import com.hendisantika.springbootquartz.repository.SchedulerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Scheduler;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-quartz
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/8/23
+ * Time: 10:04
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class SchedulerJobService {
+
+    private final Scheduler scheduler;
+
+    private final SchedulerFactoryBean schedulerFactoryBean;
+
+    private final SchedulerRepository schedulerRepository;
+
+    private final ApplicationContext context;
+
+    private final JobScheduleCreator scheduleCreator;
+}
